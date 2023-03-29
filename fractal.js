e991a909de601220ca15ea38734ed0edf7d1af6a
XTOTAM <arohalya@gmail.com>
2023-03-29 15:42:43 +0300

    fractal

---
+++ fractal.js
@@ -0,0 +1,98 @@
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 1, 1000);
+camera.position.z = 1;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.PlaneGeometry(2, 2);
+const material = new THREE.ShaderMaterial({
+  uniforms: {
+    zoom: { value: 1.0 },
+    center: { value: new THREE.Vector2(0, 0) },
+  },
+  vertexShader: `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: `
+    varying vec2 vUv;
+    uniform float zoom;
+    uniform vec2 center;
+
+    const int maxIter = 1000;
+    const float escapeRadius = 4.0;
+
+    vec3 colorize(float t) {
+      return vec3(0.5 * cos(6.28318 * (t + 0.0)) + 0.5, 0.5 * cos(6.28318 * (t + 0.33)) + 0.5, 0.5 * cos(6.28318 * (t + 0.67)) + 0.5);
+    }
+
+    void main() {
+      vec2 uv = (vUv - 0.5) * 3.0;
+      uv /= zoom;
+      uv += center;
+
+      vec2 c = uv;
+      vec2 z = vec2(0.0);
+
+      int i;
+      for (i = 0; i < maxIter; i++) {
+        float x = (z.x * z.x - z.y * z.y) + c.x;
+        float y = (z.y * z.x + z.x * z.y) + c.y;
+        z = vec2(x, y);
+        if (dot(z, z) > escapeRadius) break;
+      }
+
+      float t = float(i) / float(maxIter);
+      vec3 color = colorize(t);
+      gl_FragColor = vec4(color, 1.0);
+    }
+  `,
+});
+
+const plane = new THREE.Mesh(geometry, material);
+scene.add(plane);
+
+renderer.render(scene, camera);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+function onWheel(event) {
+  event.preventDefault();
+
+  // Calculate mouse position in normalized device coordinates (-1 to +1) for both components
+  const mouse = new THREE.Vector2();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Calculate the corresponding position in the fractal
+  const fractalPos = new THREE.Vector2();
+  fractalPos.x = (mouse.x * 3.0) / material.uniforms.zoom.value + material.uniforms.center.value.x;
+  fractalPos.y = (mouse.y * 3.0) / material.uniforms.zoom.value + material.uniforms.center.value.y;
+
+  const delta = Math.sign(event.deltaY);
+  const zoomFactor = 1.1;
+  material.uniforms.zoom.value *= delta > 0 ? zoomFactor : 1 / zoomFactor;
+
+  // Update the center based on the mouse pointer coordinates
+  material.uniforms.center.value.lerp(fractalPos, delta > 0 ? 1 - 1 / zoomFactor : 1 - zoomFactor);
+}
+
+renderer.domElement.addEventListener('wheel', onWheel, { passive: false });
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
