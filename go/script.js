326f42c70067657cc57c77bcf86d23790654ca05
XTOTAM <arohalya@gmail.com>
2023-03-30 16:34:47 +0300

    куь

--- go/script.js
+++ go/script.js
@@ -17,6 +17,9 @@ canvas.addEventListener('click', (e) => {
   if (isEmpty(row, col)) {
     boardState[row][col] = currentPlayer;
     drawStone(row, col, currentPlayer);
+
+    removeSurroundedClusters(boardState, currentPlayer);
+
     currentPlayer = currentPlayer === 'black' ? 'white' : 'black';
   }
 });
@@ -69,6 +72,112 @@ function drawStone(row, col, color) {
   ctx.fillStyle = color;
   ctx.fill();
   ctx.stroke();
+}
+
+function removeSurroundedClusters(board, player) {
+  const opponent = player === 'black' ? 'white' : 'black';
+  const visited = createEmptyBoard(gridSize);
+  let clustersToRemove = [];
+
+  for (let row = 0; row < gridSize; row++) {
+    for (let col = 0; col < gridSize; col++) {
+      if (board[row][col] === opponent && !visited[row][col]) {
+        const cluster = getCluster(board, row, col, opponent, visited);
+        const liberties = countLiberties(board, cluster);
+
+        if (liberties === 0) {
+          clustersToRemove.push(cluster);
+        }
+      }
+    }
+  }
+
+  if (clustersToRemove.length > 0) {
+    clustersToRemove.forEach(cluster => removeCluster(board, cluster));
+    drawBoard();
+    drawStones(board);
+  }
+}
+
+function removeCluster(board, cluster) {
+  cluster.forEach(({ row, col }) => {
+    board[row][col] = null;
+    ctx.clearRect(col * cellSize, row * cellSize, cellSize, cellSize);
+    drawGridIntersection(row, col);
+  });
+}
+
+function drawGridIntersection(row, col) {
+  ctx.beginPath();
+  ctx.moveTo(col * cellSize, row * cellSize + cellSize / 2);
+  ctx.lineTo(col * cellSize + cellSize, row * cellSize + cellSize / 2);
+  ctx.moveTo(col * cellSize + cellSize / 2, row * cellSize);
+  ctx.lineTo(col * cellSize + cellSize / 2, row * cellSize + cellSize);
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 1;
+  ctx.stroke();
+}
+
+function getCluster(board, row, col, color) {
+  const cluster = [];
+  const visited = createEmptyBoard(gridSize);
+
+  function visit(r, c) {
+    if (!isValid(r, c) || visited[r][c] || board[r][c] !== color) {
+      return;
+    }
+
+    cluster.push({ row: r, col: c });
+    visited[r][c] = true;
+
+    visit(r - 1, c);
+    visit(r + 1, c);
+    visit(r, c - 1);
+    visit(r, c + 1);
+  }
+
+  visit(row, col);
+
+  // Mark visited cells for all connected clusters
+  cluster.forEach(({ row, col }) => {
+    visited[row][col] = true;
+  });
+
+  return cluster;
+}
+
+
+function countLiberties(board, cluster) {
+  const liberties = new Set();
+
+  cluster.forEach(({ row, col }) => {
+    [
+      { row: row - 1, col },
+      { row: row + 1, col },
+      { row, col: col - 1 },
+      { row, col: col + 1 },
+    ].forEach(({ row, col }) => {
+      if (isValid(row, col) && isEmpty(row, col)) {
+        liberties.add(`${row}-${col}`);
+      }
+    });
+  });
+
+  return liberties.size;
+}
+
+function isValid(row, col) {
+  return row >= 0 && row < gridSize && col >= 0 && col < gridSize;
+}
+
+function drawStones(board) {
+  for (let row = 0; row < gridSize; row++) {
+    for (let col = 0; col < gridSize; col++) {
+      if (board[row][col] !== null) {
+        drawStone(row, col, board[row][col]);
+      }
+    }
+  }
 }
 
 drawBoard();
